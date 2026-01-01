2744ff902150cba9db50fbb3b5a420ec912375d2
Mahmoud Emam <laststand2231@gmail.com>
2026-01-02 01:48:39 +0200

    (feat): added empty main window to be modified later

--- ui/main_window.py
+++ ui/main_window.py
@@ -13,3 +13,17 @@
 # Rules:
 # ❌ No DB queries
 # ❌ No business rules
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Main Dashboard")
+        self.setFixedSize(800, 500)
+
+        layout = QVBoxLayout(self)
+        label = QLabel("Welcome to the Main Window")
+        label.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(label)
